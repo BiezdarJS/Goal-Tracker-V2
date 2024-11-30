ace1f276a9231884595606f5b385f3ef15ad528a
BiezdarJS <piotrdeja.pl@gmail.com>
2024-11-30 21:40:45 +0100

    [GT-V2-api]->Add main api service

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyCCMmT4PGx78cCxO9UcreCIqNzc6WfG5_o",
+    authDomain: "goal-mangement-system.firebaseapp.com",
+    databaseURL: "https://goal-mangement-system-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "goal-mangement-system",
+    storageBucket: "goal-mangement-system.firebasestorage.app",
+    messagingSenderId: "162252098870",
+    appId: "1:162252098870:web:8d1d4f247a505a6e167797",
+    measurementId: "G-YST5EMQEC4"
+  }
+}
