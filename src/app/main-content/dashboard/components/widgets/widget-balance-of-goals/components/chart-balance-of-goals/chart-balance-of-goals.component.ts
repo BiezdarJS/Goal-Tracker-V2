45441b6bcfe0b09eb50fb4284ae98bd8f650d75d
BiezdarJS <piotrdeja.pl@gmail.com>
2024-12-01 19:48:46 +0100

    [GT-V2-balance-of-goals-widget]->Integrate BE with Balance of Goals widget

--- src/app/main-content/dashboard/components/widgets/widget-balance-of-goals/components/chart-balance-of-goals/chart-balance-of-goals.component.ts
+++ src/app/main-content/dashboard/components/widgets/widget-balance-of-goals/components/chart-balance-of-goals/chart-balance-of-goals.component.ts
@@ -1,22 +1,29 @@
-import { Component, computed, inject, Input, OnInit, Signal } from '@angular/core';
+import { Component, computed, effect, inject, Signal } from '@angular/core';
 import { BalanceOfGoalsChartConfig } from './const/balance-of-goals-chart.config';
 import { ChartData } from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 import { ChartConfigService } from '@gtSharedServices/chart-config.service';
 import { ChartDataStoreService } from '@gtSharedServices/chart-data-store.service';
 import { IChartColorPalette } from '@gtSharedInterfaces/chart-color-palette.interface';
+import { DataStoreService } from '@gtSharedServices/data-store.service';
 /** Komponent wykresu wewnątrz BalanceOfGoals */
 @Component({
   selector: 'gt-chart-balance-of-goals',
   standalone: true,
   imports: [BaseChartDirective],
-  providers: [ChartConfigService, ChartDataStoreService],
+  providers: [ChartConfigService, ChartDataStoreService, DataStoreService],
   templateUrl: './chart-balance-of-goals.component.html',
   styleUrl: './chart-balance-of-goals.component.scss'
 })
-export class ChartBalanceOfGoalsComponent implements OnInit {
-  /** Obiekt zawierający wszystkie cele */
-  @Input() allGoals: any;
+export class ChartBalanceOfGoalsComponent {
+  /** Serwis przechowujący dane dla celów, tasków, itd. */
+  private dataStoreService = inject(DataStoreService);
+  /** Serwis konfiguracji dla wykresów */
+  chartConfigService = inject(ChartConfigService);
+  /** Serwis przechowywujący globalne dane dla wykresów  */
+  private chartDataStoreService = inject(ChartDataStoreService);
+  /** Liczba obecnie występujących celów - publiczne */
+  public goalsNumber: Signal<number> = computed(() => this.dataStoreService.goalsNumber());
   /** Obecnie wyświetlany motyw */
   activeTheme!: string | null;
   /** Obecnie wyświetlany motyw */
@@ -25,22 +32,22 @@ export class ChartBalanceOfGoalsComponent implements OnInit {
   protected readonly balanceOfGoalsChartConfig = BalanceOfGoalsChartConfig;
   /** Dane dla wykresu BalanceOfGoals */
   balanceOfGoalsData!: ChartData<'doughnut'>;
-  /** Serwis konfiguracji dla wykresów */
-  chartConfigService = inject(ChartConfigService);
-  /** Serwis przechowywujący globalne dane dla wykresów  */
-  private chartDataStoreService = inject(ChartDataStoreService);
   /** Obecnie wybrane kolory  */
   public chartColors: Signal<IChartColorPalette | null> = computed(() => this.chartDataStoreService.chartColors());
 
 
-  ngOnInit(): void {
-    this.initBalanceOfGoalsData();
+  constructor() {
+    effect(() => {
+      if (this.goalsNumber()) {
+        this.initBalanceOfGoalsData();
+      }
+    });
   }
 
   /** Dane dla wykresu BalanceOfGoals */
   private initBalanceOfGoalsData(): void {
     this.balanceOfGoalsData = {
-      labels: [3, 'GOALS'],
+      labels: [this.goalsNumber(), 'GOALS'],
       datasets: [
         {
           data: [300, 50, 100],
--- src/app/main-content/dashboard/components/widgets/widget-balance-of-goals/widget-balance-of-goals.component.ts
+++ src/app/main-content/dashboard/components/widgets/widget-balance-of-goals/widget-balance-of-goals.component.ts
@@ -1,15 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, Signal } from '@angular/core';
 import { BalanceListComponent } from './components/balance-list/balance-list.component';
 import { ChartBalanceOfGoalsComponent } from './components/chart-balance-of-goals/chart-balance-of-goals.component';
+import { DataStoreService } from '@gtSharedServices/data-store.service';
 /** Komponent widgetu BalanceOfGoals w dashboard */
 @Component({
   selector: 'gt-widget-balance-of-goals',
   standalone: true,
   imports: [ChartBalanceOfGoalsComponent, BalanceListComponent],
+  providers: [DataStoreService],
   templateUrl: './widget-balance-of-goals.component.html',
   styleUrl: './widget-balance-of-goals.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WidgetBalanceOfGoalsComponent {
-
+  
 }
